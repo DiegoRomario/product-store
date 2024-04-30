5718f2b59f4cfb335e208f312d8f65ac7135ebe7
Diego Romario <diego.romario@outlook.com>
2024-04-30 11:18:13 -0300

    feat: add create product component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
     {
         path: '',
         component: ListComponent
+    },
+    {
+        path: 'create-product',
+        loadComponent: () => import('./features/create/create.component').then(m => m.CreateComponent)
     }
 ];
