a9bff5d6bf76ec6c54a6f174e29bc417a8a06eba
Diego Romario <diego.romario@outlook.com>
2024-04-30 10:45:51 -0300

    feat: add product card

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-
+import { CardComponent } from './card/card.component';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
